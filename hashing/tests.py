86a493cab3dd39f12c4d43c4cf6bdf49580badc5
Navid Dezashibi <dezashibi@gmail.com>
2020-08-05 15:00:46 +0430

    functional test implemented

--- hashing/tests.py
+++ hashing/tests.py
@@ -6,9 +6,16 @@ class FunctionalTestCase(TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
 
-    def test_there_is_gome_page(self):
+    def test_there_is_home_page(self):
         self.browser.get('http://localhost:8000')
-        self.assertIn("install", self.browser.page_source)
+        self.assertIn("Enter hash here:", self.browser.page_source)
+
+    def test_hash_of_hello(self):
+        self.browser.get('http://localhost:8000')
+        text = self.browser.find_element_by_id('id_text')
+        text.send_keys('hello')
+        self.browser.find_element_by_name('submit').click()
+        self.assertIn('2CF24DBA5FB0A30E26E83B2AC5B9E29E1B161E5C1FA7425E73043362938B9824', self.browser.page_source)
 
     def tearDown(self):
         self.browser.quit()
