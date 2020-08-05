24ad92a88af65231192df60f4a6918e4c150315c
Navid Dezashibi <dezashibi@gmail.com>
2020-08-05 15:25:10 +0430

    TDD unit test for hashing and home page

--- hashing/tests.py
+++ hashing/tests.py
@@ -1,23 +1,39 @@
 from django.test import TestCase
 from selenium import webdriver
+from .form import HashForm
+import hashlib
 
-class FunctionalTestCase(TestCase):
+# class FunctionalTestCase(TestCase):
+#
+#     def setUp(self):
+#         self.browser = webdriver.Chrome()
+#
+#     def test_there_is_home_page(self):
+#         self.browser.get('http://localhost:8000')
+#         self.assertIn("Enter hash here:", self.browser.page_source)
+#
+#     def test_hash_of_hello(self):
+#         self.browser.get('http://localhost:8000')
+#         text = self.browser.find_element_by_id('id_text')
+#         text.send_keys('hello')
+#         self.browser.find_element_by_name('submit').click()
+#         self.assertIn('2CF24DBA5FB0A30E26E83B2AC5B9E29E1B161E5C1FA7425E73043362938B9824', self.browser.page_source)
+#
+#     def tearDown(self):
+#         self.browser.quit()
 
-    def setUp(self):
-        self.browser = webdriver.Chrome()
 
-    def test_there_is_home_page(self):
-        self.browser.get('http://localhost:8000')
-        self.assertIn("Enter hash here:", self.browser.page_source)
+class UnitTestCase(TestCase):
 
-    def test_hash_of_hello(self):
-        self.browser.get('http://localhost:8000')
-        text = self.browser.find_element_by_id('id_text')
-        text.send_keys('hello')
-        self.browser.find_element_by_name('submit').click()
-        self.assertIn('2CF24DBA5FB0A30E26E83B2AC5B9E29E1B161E5C1FA7425E73043362938B9824', self.browser.page_source)
+    def test_home_homepage_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'hashing/home.html')
 
-    def tearDown(self):
-        self.browser.quit()
+    def test_hash_form(self):
+        form = HashForm(data={'text': 'hello'})
+        self.assertTrue(form.is_valid)
 
+    def test_hash_function_works_properly(self):
+        text_hash = hashlib.sha256('hello'.encode('utf-8')).hexdigest()
+        self.assertEqual('2CF24DBA5FB0A30E26E83B2AC5B9E29E1B161E5C1FA7425E73043362938B9824'.lower(), text_hash)
 
--- hashing/views.py
+++ hashing/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .form import HashForm
+
+
+def home(request):
+    form = HashForm()
+    return render(request, 'hashing/home.html', {'form': form})
