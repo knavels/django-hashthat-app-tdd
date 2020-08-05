886fa138e09f2e9683e2d2aa365ed965da381d27
Navid Dezashibi <dezashibi@gmail.com>
2020-08-05 14:51:48 +0430

    Initial commit - TDD - functional test

---
+++ hashing/tests.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from selenium import webdriver
+
+class FunctionalTestCase(TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def test_there_is_gome_page(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn("install", self.browser.page_source)
+
+    def tearDown(self):
+        self.browser.quit()
+
+
